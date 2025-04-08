257c73954233ca26030b5c4b1508030c8eb29c04
inevwv <laujoycek@gmail.com>
2025-04-07 19:13:53 -0500

    add avian flu dataset

--- data_cleaning.R
+++ data_cleaning.R
@@ -1,4 +1,8 @@
 # data_cleaning.R
+source("install_and_load.R")
+install_and_load(
+  "dplyr"
+)
 
 # Load datasets
 gdp_data <- read.csv("data_sources/gdp_by_year.csv")
@@ -6,6 +10,7 @@ health_expenditure <- read.csv("data_sources/health_expenditure_per_capita.csv")
 hospital_beds <- read.csv("data_sources/hospital_bed_per_1000.csv")
 exp_gdp_ratio <- read.csv("data_sources/health_exp_gdp_ratio.csv")
 poultry_prod <- read.csv("data_sources/poultry-production-tonnes/poultry-production-tonnes.csv")
+H5N1_incidence <- read.csv("data_sources/h5n1-flu-reported-cases/h5n1-flu-reported-cases.csv")
 
 # Filter data for the countries you're interested in
 countries_of_interest <- c("USA", "GBR", "VNM", "CHN")
@@ -22,6 +27,10 @@ health_exp_subset <- subset(health_expenditure, Country.Code %in% countries_of_i
 hosp_beds_subset <- subset(hospital_beds, Country.Code %in% countries_of_interest)[, c("Country.Name", "X2004", "X2005", "X2006", "X2007", "X2008", "X2009", "X2010")]
 exp_gdp_ratio_subset <- subset(exp_gdp_ratio, Country.Code %in% countries_of_interest)[, c("Country.Name", "X2004", "X2005", "X2006", "X2007", "X2008", "X2009", "X2010")]
 poultry_prod_subset <- subset(poultry_prod, Country.Code %in% countries_of_interest & Year %in% 2004:2010)
+H5N1_incidence_subset <- H5N1_incidence %>%
+  subset(Country.Code %in% countries_of_interest & Year %in% 2004:2010) %>%
+  group_by(Country.Name, Year) %>%
+  summarise(Total.Cases = sum(Human.Cases, na.rm = TRUE))
 
 if (!dir.exists("working_data")) {
   dir.create("working_data")
@@ -33,10 +42,11 @@ write.csv(health_exp_subset, "working_data/health_exp_subset.csv", row.names = F
 write.csv(hosp_beds_subset, "working_data/hosp_beds_subset.csv", row.names = FALSE)
 write.csv(exp_gdp_ratio_subset, "working_data/exp_gdp_ratio_subset.csv", row.names = FALSE)
 write.csv(poultry_prod_subset, "working_data/poultry_prod_subset.csv", row.names = FALSE)
+write.csv(H5N1_incidence_subset, "working_data/H5N1_incidence_subset.csv", row.names = FALSE)
 
 
 # Clean up environment by removing raw datasets
-rm(gdp_data, health_expenditure, hospital_beds, exp_gdp_ratio, poultry_prod)
+rm(gdp_data, health_expenditure, hospital_beds, exp_gdp_ratio, poultry_prod, H5N1_incidence)
 
 
 # Print message to confirm
