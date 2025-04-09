8e224cb424cd01d1e9c294811fe148148590d949
inevwv <inev.lau@gmail.com>
2025-04-09 01:47:43 -0500

    add data, create data

---
+++ testing_access.R
@@ -0,0 +1,9 @@
+# determine testing access
+source("install_and_load.R")
+install_and_load(
+  "dplyr",
+  "readr"
+)
+
+# automate scoring --> determine thresholds for the proxies
+# average scores by year = testing access
